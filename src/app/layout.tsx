a31bd88108c3bbb3c251a2653b1315d7cdb3f874
Sourabh Magadum <sourabhmagadum528@gmail.com>
2026-01-14 18:06:25 +0000

    always keep it dark when they need they will turn it into light

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       <body className={cn('font-body antialiased')}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
         >
