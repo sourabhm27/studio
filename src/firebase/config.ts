3db79cb1d0cd17fd0289a3f9107ba817f4cf7f18
Sourabh Magadum <sourabhmagadum528@gmail.com>
2026-01-14 16:55:52 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5615753745-cf42a",
+  "appId": "1:819533210566:web:224e7f2cda0d36b10008c7",
+  "apiKey": "AIzaSyBBPWX5u98hmAMjnOl5jWv_s44tVeUMzgM",
+  "authDomain": "studio-5615753745-cf42a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "819533210566"
+};
